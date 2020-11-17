9e697434132b835391c6c5135fb5e6097e173bcd
alibulut <alibulut96@gmail.com>
2020-11-17 22:43:56 +0100

    new activity design added

--- app/src/main/java/com/example/shoppingchart/OptionsActivity.kt
+++ app/src/main/java/com/example/shoppingchart/OptionsActivity.kt
@@ -1,4 +1,15 @@
 package com.example.shoppingchart
 
-class OptionsActivity {
+import android.os.Bundle
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class OptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        var fontSize = findViewById<Spinner>(R.id.spinnerFontSize)
+        var fontColor = findViewById<Spinner>(R.id.spinnerFontColor)
+
+    }
 }
